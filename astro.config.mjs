aa24f0bdc299dc7f924d79d988783194fa562a92
joordonezo <jorgeluisordonez1995@gmail.com>
2025-05-11 21:12:30 -0500

    add SEO config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://vegetalyfoods.com',
+  site: "https://vegetalyfoods.com",
+  integrations: [sitemap()],
 });
